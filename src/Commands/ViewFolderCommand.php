6f3c13d08112680ff20a5373b4c3479d036d0b9d
Brian Tweeed <brian@thetomorrowlab.com>
2018-10-16 08:44:46 +0100

    stub refactor. interface created for consts

--- src/Commands/ViewFolderCommand.php
+++ src/Commands/ViewFolderCommand.php
@@ -5,40 +5,27 @@ namespace briantweed\LaravelViewFolder\Commands;
 use Illuminate\Support\Facades\File;
 use Illuminate\Console\GeneratorCommand;
 
-class ViewFolderCommand extends GeneratorCommand
+use App\Interfaces\ViewFolderInterface;
+
+
+class ViewFolderCommand extends GeneratorCommand implements ViewFolderInterface
 {
 
     protected $signature = 'make:view {path?} {{--p : Add partials sub-folder}}  {{--m : Add modals sub-folder}}  {{--c : Add components sub-folder}} {{--f : Add CRUD files}}';
+    
     protected $description = 'Create view folder';
+    
     protected $type = 'Console command';
 
-    protected $additionalFiles;
-    protected $optionGiven = false;
     protected $basePath;
+    
     protected $currentFolder;
-
-    protected const RESOURCE_PATH = './resources/views/';
-
-    protected const FOLDER_PATH_QUESTION = 'What is the folder path (use can use forward slashes or dots) ?';
-    protected const SUBFOLDER_QUESTION = 'Would you like a sub-folder for ';
-    protected const CRUD_QUESTION = 'Would you like to add CRUD files?';
-
-    protected const PARTIALS_FOLDER = 'partials';
-    protected const MODALS_FOLDER = 'modals';
-    protected const COMPONENTS_FOLDER = 'components';
-
-    protected const OPTIONS_ARRAY = ['p','c','m','f'];
-
-    protected const CRUD_FILES = [
-        'index',
-        'create',
-        'edit',
-        'delete'
-    ];
-
-    protected const SUCCESS_MESSAGE = 'View folder created';
-    protected const ALREADY_EXISTS_MESSAGE = 'Folder already exists';
-
+    
+    protected $stubFile;
+    
+    protected $additionalFiles;
+    
+    protected $optionGiven = false;
 
 
     public function handle()
@@ -53,19 +40,23 @@ class ViewFolderCommand extends GeneratorCommand
 
             $askQuestions = !$this->additionalOptionsGiven();
 
-            if ($this->option('p') || ($askQuestions && $this->confirm(self::SUBFOLDER_QUESTION . self::PARTIALS_FOLDER . '?'))) {
+            if ($this->option('p') || ($askQuestions && $this->confirm(self::SUB_FOLDER_QUESTION . self::PARTIALS_FOLDER . '?'))) {
+                $this->stubFile = self::SUB_FOLDER_STUB;
                 $this->createDirectory(self::PARTIALS_FOLDER);
             }
 
-            if ($this->option('m') || ($askQuestions && $this->confirm(self::SUBFOLDER_QUESTION . self::MODALS_FOLDER . '?'))) {
+            if ($this->option('m') || ($askQuestions && $this->confirm(self::SUB_FOLDER_QUESTION . self::MODALS_FOLDER . '?'))) {
+                $this->stubFile = self::SUB_FOLDER_STUB;
                 $this->createDirectory(self::MODALS_FOLDER);
             }
 
-            if ($this->option('c') || ($askQuestions && $this->confirm(self::SUBFOLDER_QUESTION . self::COMPONENTS_FOLDER . '?'))) {
+            if ($this->option('c') || ($askQuestions && $this->confirm(self::SUB_FOLDER_QUESTION . self::COMPONENTS_FOLDER . '?'))) {
+                $this->stubFile = self::SUB_FOLDER_STUB;
                 $this->createDirectory(self::COMPONENTS_FOLDER);
             }
 
             $this->setCurrentFolder();
+            $this->stubFile = self::CRUD_FILE_STUB;
 
             if ($this->option('f') || ($askQuestions && $this->confirm(self::CRUD_QUESTION))) {
                 $this->createCrudFiles();
@@ -85,7 +76,7 @@ class ViewFolderCommand extends GeneratorCommand
     }
 
 
-    protected function createDirectory($folder)
+    protected function createDirectory(string $folder)
     {
         $this->setCurrentFolder($folder);
 
@@ -94,26 +85,20 @@ class ViewFolderCommand extends GeneratorCommand
         $additionalFiles = $this->ask('Create files for ' . strtolower($folder) . ' folder (comma separated) ?', false);
 
         if($additionalFiles) {
-            $this->createAdditionalFiles($additionalFiles, false);
+            $this->createAdditionalFiles($additionalFiles);
         }
     }
 
 
-    protected function getCrudpageStub(): string
+    protected function getStub(): string
     {
-        return __DIR__.'/stubs/crud-page.stub';
-    }
-
-
-    protected function getSubpageStub(): string
-    {
-        return __DIR__.'/stubs/subfolder-page.stub';
+        return __DIR__.'/stubs/' . $this->stubFile . '.stub';
     }
 
 
     private function setBasePath()
     {
-        $this->basePath = self::RESOURCE_PATH . $this->formatPath();
+        $this->basePath = self::RESOURCE_PATH . $this->formatPath($this->basePath);
     }
 
 
@@ -123,13 +108,7 @@ class ViewFolderCommand extends GeneratorCommand
     }
 
 
-    private function formatPath(): string
-    {
-        return $this->format($this->basePath);
-    }
-
-
-    private function format(string $string): string
+    private function formatPath(string $string): string
     {
         return str_replace('.', '/', $string);
     }
@@ -143,11 +122,11 @@ class ViewFolderCommand extends GeneratorCommand
     }
 
 
-    private function createAdditionalFiles(string $files, $useStub = true)
+    private function createAdditionalFiles(string $files)
     {
         $filesArray = $this->getFiles($files);
         foreach($filesArray as $file) {
-            $this->createFile($file, $useStub);
+            $this->createFile($file);
         }
     }
 
@@ -158,13 +137,9 @@ class ViewFolderCommand extends GeneratorCommand
     }
 
 
-    /**
-     * @todo refactor so not passing boolean to determine which stub to use
-     */
-    private function createFile(string $file, $useStub = true)
+    private function createFile(string $file)
     {
-        $template = $useStub === true ? $this->getCrudpageStub() : $this->getSubpageStub();
-        File::put($this->basePath . '/' . $this->currentFolder . $file . '.blade.php', $this->files->get($template));
+        File::put($this->basePath . '/' . $this->currentFolder . $file . '.blade.php', $this->files->get($this->getStub()));
     }
 
 
---
+++ src/Commands/interfaces/ViewFolderInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Interfaces;
+
+interface ViewFolderInterface
+{
+    public const RESOURCE_PATH = './resources/views/';
+
+    public const FOLDER_PATH_QUESTION = 'What is the folder path (use can use forward slashes or dots) ?';
+    public const SUB_FOLDER_QUESTION = 'Would you like a sub-folder for ';
+    public const CRUD_QUESTION = 'Would you like to add CRUD files?';
+
+    public const PARTIALS_FOLDER = 'partials';
+    public const MODALS_FOLDER = 'modals';
+    public const COMPONENTS_FOLDER = 'components';
+
+    public const CRUD_FILE_STUB = 'crud-page';
+    public const SUB_FOLDER_STUB = 'sub-folder-page';
+
+    public const OPTIONS_ARRAY = ['p','c','m','f'];
+
+    public const CRUD_FILES = [
+        'index',
+        'create',
+        'edit',
+        'delete'
+    ];
+
+    public const SUCCESS_MESSAGE = 'View folder created';
+    public const ALREADY_EXISTS_MESSAGE = 'Folder already exists';
+    
+}
